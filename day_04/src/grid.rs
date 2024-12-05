baa0239c3c842d6cb4e3cabcd252bee6d2d259da
Josiah Hunsinger <fencepost13302@gmail.com>
2024-12-05 15:07:27 -0500

    Finished day 4

---
+++ day_04/src/grid.rs
@@ -0,0 +1,134 @@
+use std::ops::{Mul, Add, Sub, Index};
+use derive_more::Display;
+
+// Error / Result
+#[derive(Display, Debug)]
+pub enum Error {
+    InconsitantRowLengths
+}
+
+impl std::error::Error for Error {}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+// GridVec
+#[derive(Clone, Copy)]
+pub struct GridVec {
+    pub i: i32,
+    pub j: i32
+}
+
+impl GridVec {
+    pub fn rot_90(&self) -> GridVec {
+        GridVec {
+            i: -self.j,
+            j: self.i
+        }
+    }
+}
+
+impl From<[i32; 2]> for GridVec {
+    fn from(value: [i32; 2]) -> Self {
+        GridVec { i: value[0], j: value[1] }
+    }
+}
+
+impl From<[usize; 2]> for GridVec {
+    fn from(value: [usize; 2]) -> Self {
+        GridVec { i: value[0] as i32, j: value[1] as i32 }
+    }
+}
+
+impl Mul<i32> for GridVec {
+    type Output = GridVec;
+
+    fn mul(self, rhs: i32) -> Self::Output {
+        GridVec { 
+            i: self.i * rhs, 
+            j: self.j * rhs
+        }
+    }
+}
+
+impl Mul<GridVec> for i32 {
+    type Output = GridVec;
+
+    fn mul(self, rhs: GridVec) -> Self::Output {
+        rhs*self
+    }
+}
+
+impl Add for GridVec {
+    type Output = GridVec;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        GridVec {
+            i: self.i + rhs.i,
+            j: self.j + rhs.j
+        }
+    }
+}
+
+impl Sub for GridVec {
+    type Output = GridVec;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        self + -1*rhs
+    }
+}
+
+// Grid
+pub struct Grid<T> {
+    pub rows: Vec<Vec<T>>,
+    pub row_count: usize,
+    pub col_count: usize
+}
+
+impl<T> Grid<T> {
+    pub fn from_iter(row_it: impl Iterator<Item = impl Iterator<Item = T>>) -> Result<Self> {
+        let mut grid = Grid {
+            rows: Vec::new(),
+            row_count: 0,
+            col_count: 0
+        };
+        let mut first_row = true;
+        for item_it in row_it {
+            let mut row = vec![];
+            for item in item_it {
+                row.push(item); 
+            }
+
+            if first_row {
+                grid.col_count = row.len();
+                first_row = false;
+            } else if !first_row && row.len() != grid.col_count {
+                return Err(Error::InconsitantRowLengths);
+            }
+
+            grid.rows.push(row)
+        }
+        grid.row_count = grid.rows.len();
+
+        Ok(grid)
+    }
+
+    pub fn get(&self, loc: GridVec) -> Option<&T> {
+        if loc.i < 0
+            || loc.j < 0 
+            || loc.i as usize >= self.row_count 
+            || loc.j as usize >= self.col_count 
+        {
+            return None
+        }
+
+        Some(&self.rows[loc.i as usize][loc.j as usize])
+    }
+}
+
+impl<T> Index<GridVec> for Grid<T> {
+    type Output = T;
+
+    fn index(&self, index: GridVec) -> &Self::Output {
+        self.get(index).unwrap()
+    }
+}
---
+++ day_04/src/main.rs
@@ -0,0 +1,95 @@
+#![feature(let_chains)]
+
+mod grid;
+use grid::*;
+
+const PUZZLE_INPUT: &str = include_str!("../puzzle_input.txt");
+
+fn main() {
+    println!("Part 1 Answer: {}", solve_part_1(PUZZLE_INPUT));
+    println!("Part 2 Answer: {}", solve_part_2(PUZZLE_INPUT));
+}
+
+fn parse_grid(input: &str) -> Grid<char> { 
+    Grid::from_iter(input.lines().map(|l| l.trim().chars())).unwrap()
+}
+
+fn solve_part_1(input: &str) -> u32 {
+    let grid = parse_grid(input);
+    let dirs = (-1..=1).into_iter()
+        .flat_map(|i| (-1..=1).into_iter().map(move |j| [i, j]))
+        .filter(|v| *v != [0, 0])
+        .map(|v| GridVec::from(v))
+        .collect::<Vec<_>>();
+
+    let mut match_count = 0;
+    for i in 0..grid.row_count {
+        for j in 0..grid.col_count {
+            let offset = GridVec::from([i, j]);
+            if grid[offset] == 'X' {
+                for &dir in dirs.iter() {
+                    if let Some(m) = grid.get(offset + 1*dir) && *m == 'M'
+                        && let Some(a) = grid.get(offset + 2*dir) && *a == 'A'
+                        && let Some(s) = grid.get(offset + 3*dir) && *s == 'S'    
+                    {
+                        match_count += 1
+                    }
+                }
+            }
+        } 
+    }
+
+    match_count
+}
+
+fn solve_part_2(input: &str) -> u32 {
+    let grid = parse_grid(input);
+
+    let corner = GridVec::from([1, 1]);
+    let corners = [
+        corner,
+        corner.rot_90(),
+        corner.rot_90().rot_90(),
+        corner.rot_90().rot_90().rot_90()
+    ];
+    let corner_pairs = corners.into_iter()
+        .map(|c| [c, c.rot_90()])
+        .collect::<Vec<_>>();
+
+    let mut match_count = 0;
+    for i in 0..grid.row_count {
+        for j in 0..grid.col_count {
+            let offset = GridVec::from([i, j]);
+            if grid[offset] == 'A' {
+                for &[corner1, corner2] in corner_pairs.iter() {
+                    if let Some(m1) = grid.get(offset + corner1) && *m1 == 'M'
+                        && let Some(m2) = grid.get(offset + corner2) && *m2 == 'M'
+                        && let Some(s1) = grid.get(offset - corner1) && *s1 == 'S'
+                        && let Some(s2) = grid.get(offset - corner2) && *s2 == 'S'
+                    {
+                        match_count += 1;
+                    }
+                }
+            }
+        } 
+    }
+
+    match_count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = include_str!("../test_input.txt");
+    
+    #[test]
+    fn test_solve_part_1() {
+        assert_eq!(solve_part_1(TEST_INPUT), 18);     
+    }
+
+    #[test]
+    fn test_solve_part_2() {
+        assert_eq!(solve_part_2(TEST_INPUT), 9);     
+    }
+}
