330d5993eb80edd8c98405c3a050da710edda0d1
Josiah Hunsinger <fencepost13302@gmail.com>
2024-12-09 14:40:30 -0500

    Added boilerplate.rs to save time on future days

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -2,12 +2,21 @@ const PUZZLE_INPUT: &str = include_str!("../puzzle_input.txt");
 
 fn main() {
     println!("Part 1 Answer: {}", solve_part_1(PUZZLE_INPUT));
+    println!("Part 2 Answer: {}", solve_part_2(PUZZLE_INPUT));
 }
 
 fn solve_part_1(input: &str) -> usize {
     let eqs = parse_puzzle(input);
     eqs.iter()
         .filter(|eq| can_eq(eq.0, eq.1[0], &eq.1[1..]))
+        .map(|eq| eq.0)
+        .sum()
+}
+
+fn solve_part_2(input: &str) -> usize {
+    let eqs = parse_puzzle(input);
+    eqs.iter()
+        .filter(|eq| can_eq_2(eq.0, eq.1[0], &eq.1[1..]))
         .map(|eq| eq.0)
         .sum()
 }
@@ -33,6 +42,20 @@ fn can_eq(target: usize, accum: usize, remaining: &[usize]) -> bool {
     can_eq(target, accum + remaining[0], &remaining[1..]) || can_eq(target, accum * remaining[0], &remaining[1..])
 }
 
+fn can_eq_2(target: usize, accum: usize, remaining: &[usize]) -> bool {
+    if remaining.len() == 0 {
+        return accum == target
+    }
+
+    can_eq_2(target, accum + remaining[0], &remaining[1..]) 
+    || can_eq_2(target, accum * remaining[0], &remaining[1..])
+    || can_eq_2(target, cat_nums(accum, remaining[0]), &remaining[1..])
+}
+
+fn cat_nums(a: usize, b:usize) -> usize {
+    format!("{}{}", a, b).parse().unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -50,5 +73,15 @@ mod tests {
         // because I didn't think of this case where zeroing the first term can
         // make the equation solvable when it should not be.
         assert_eq!(solve_part_1("1: 10 1"), 0);
+    }
+
+    #[test]
+    fn test_solve_part_2() {
+        assert_eq!(solve_part_2(TEST_INPUT), 11387);
+    }
+
+    #[test]
+    fn test_cat_nums() {
+        assert_eq!(cat_nums(1,2), 12);
     }
 }
